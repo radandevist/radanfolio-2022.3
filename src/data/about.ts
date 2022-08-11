c7e929ea1c6827e5e44ec0f5fa485d266e2a49eb
Andrianarisoa Daniel <radandevist@gmail.com>
2022-08-11 21:08:44 +0300

    🎨 added a resume button and skills data rewrited

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,14 +1,13 @@
+import { v4 } from "uuid";
+
 /* eslint-disable max-len */
-export const about = {
+const preAbout = {
   avatar: "https://res.cloudinary.com/dhwkzyl32/image/upload/v1660190042/radanfolio/profile_pic_imdxvm.jpg",
   name: "Daniel Adriananrisoa",
   email: "radandevist@gmail.com",
   github: "https://www.github.com/radandevist",
+  resumeUrl: "https://drive.google.com/uc?export=download&id=1cdJvbW1MsjvESowf32URfAz3Vpcw2uFk",
   socials: [
-    // { 
-    //   name: "Twitter",
-    //   url: "https://www.twitter.com"
-    // },
     { 
       name: "Linkedin",
       url: "https://www.linkedin.com/in/radandevist"
@@ -17,47 +16,34 @@ export const about = {
       name: "Instagram",
       url: "https://www.instagram.com/radandevist"
     },
+    { 
+      name: "Facebook Page",
+      url: "https://www.facebook.com/radandevist"
+    },
+    { 
+      name: "Facebook Profile",
+      url: "https://www.twitter.com/radandevist.me"
+    },
   ],
   skills: [
-    {
-      id: 0,
-      name: "html"
-    },
-    {
-      id: 1,
-      name: "css"
-    },
-    {
-      id: 2,
-      name: "javascript"
-    },
-    {
-      id: 3,
-      name: "responsive design"
-    },
-    {
-      id: 4,
-      name: "bootstrap"
-    },
-    {
-      id: 5,
-      name: "tailwind"
-    },
-    {
-      id: 6,
-      name: "react"
-    },
-    {
-      id: 7,
-      name: "version control"
-    },
-    {
-      id: 8,
-      name: "github"
-    },
+    "HTML", "CSS", "Javascript", "Typescript", "React.js", "Node.js", "Express.js", "Microsoft Office", "Office computing",  "Responsive design", "Bootstrap", "Tailwind", "version control",
+    "github", "Googling", "Duckling", "Test Driven Development", "ESLint", "ESBuild", "Webpack", "Vite.js", "Imkscape", "Linux", "Socket.IO", "WebRTC", "Prisma", "Sequelize", "Mocha.js",
+    "Chai.js", "Sinon.js", "Jest"
   ],
   bioIntro: "Hey! I'm Radan",
   bioPreface: "A full-stack Javascript and Typescript 👨🏿‍💻developer from 🇲🇬 Madagascar. I'm also a 🐧Linux user in an everyday basis.",
   bioSub: "I am a mostly self-taught MERN Stack developer (despite the fact that I attended college) who values attention to detail and with a passion for solving complex problems. Offering the significant ability to work well both independently and in collaboration with close-knit teams. Building large and scalable web architectures is what I aim for.",
   // bioMain: "Banh mi you probably havent heard of them yuccie beard deep v. Salvia roof party selvage jianbing, irony forage direct trade. Pug tumeric mixtape, church-key cray single-origin coffee selvage ugh prism keytar. Typewriter yr plaid post-ironic. Palo santo twee ethical, pug squid godard jianbing adaptogen."
+};
+
+const { skills: preSkills, ...destructuredAbout } = preAbout;
+
+const skillsWithId = preSkills.map(skill => ({
+  id: v4(),
+  name: skill,
+}));
+
+export const about = {
+  ...destructuredAbout,
+  skills: skillsWithId,
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         <section className="animate animate__animated animate__fadeIn mxw-sm">
           <div className="grid gap-6 grid-cols-1 sm:grid-cols-5">
             <div className="sm:col-span-1">
-              {/* <img alt="profile_pic" className="h-full w-full 4 object-cover rounded-lg shrink-0" src={info?.avatar} /> */}
+              {/* <img alt="profile_pic" className="h-full w-full 4 object-cover rounded-lg shrink-0" src={info.avatar} /> */}
               <Image
                 height={1032}
                 width={774}
@@ -26,20 +26,21 @@ const Home: NextPage = () => {
               />
             </div>
             <div className="sm:col-span-4 flex flex-col items-start justify-center space-y-3">
-              <h2 className="text-4xl">{info?.bioIntro}</h2>
-              <p className="text-2xl font-light">{info?.bioPreface}</p>
+              <h2 className="text-4xl">{info.bioIntro}</h2>
+              <p className="text-2xl font-light">{info.bioPreface}</p>
               <div className="flex items-center space-x-4 flex-wrap justify-start">
-                {info?.socials && info?.socials.map((social) => (
-                  <a key={social?.name} target="_blank" rel="noreferrer" href={social?.url} className="inline-block my-1 bg-brand1-500 text-white font-semibold py-2 px-3">{social?.name}</a>
+                {info.socials && info.socials.map((social) => (
+                  <a key={social.name} target="_blank" rel="noreferrer" href={social.url} className="inline-block my-1 bg-brand1-500 text-white font-semibold py-2 px-3">{social?.name}</a>
                 ))}
               </div>
+              <a rel="noreferrer" href={info.resumeUrl} className="block border border-brand1-500 bo text-brand1-500 font-semibold py-2 px-3">Download ny resume</a>
             </div>
           </div>
         </section>  
         <div className="w-full my-12 max-w-[200px] mx-auto h-3 bg-gray-200" />
         <section className="mxw-sm my-12">
-          <p className="text-2xl md:text-3xl py-6">{info?.bioSub}</p>
-          {/* <p className="text-2xl md:text-3xl font-light">{info?.bioSub}</p> */}
+          <p className="text-2xl md:text-3xl py-6">{info.bioSub}</p>
+          {/* <p className="text-2xl md:text-3xl font-light">{info.bioSub}</p> */}
           {/* skills  */}
           <p className="text-2xl md:text-3xl my-12">Skills include:</p>
           <div className="flex items-center flex-wrap">
@@ -47,7 +48,7 @@ const Home: NextPage = () => {
               <span key={skill?.id} className="py-2 px-3 my-1 mx-2 shadow-lg shadow-slate-300 bg-brand1-500 text-white font-semibold">{skill?.name}</span>
             ))}
           </div>
-          {/* <p className="text-2xl md:text-3xl py-6">{info?.bioMain}</p> */}
+          {/* <p className="text-2xl md:text-3xl py-6">{info.bioMain}</p> */}
         </section>
       </div>
     </AnimatedPage>
