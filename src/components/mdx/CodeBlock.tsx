ee631dab14f9ef347181f6b1e8ca86a8f32d1d59
Andrianarisoa Daniel <radandevist@gmail.com>
2022-09-04 13:55:44 +0300

    ✨ horizontal scroll on code blocks

--- src/components/mdx/CodeBlock.tsx
+++ src/components/mdx/CodeBlock.tsx
@@ -1,25 +1,27 @@
 import { FC } from "react";
 
 export const CodeBlock: FC = ({ children }) => (
-  <div className="flex overflow-hidden relative flex-col
-      p-5 mx-auto my-5 space-y-5 w-full font-mono
-      text-lg md:text-xl
-      rounded-lg bg-brand2-500
-      !mt-0">
-    {/* text-2xl md:text-3xl font-light first-letter:text-4xl */}
-    <div className="flex space-x-3 w-full">
+  <div className="
+  bg-brand2-500 rounded-lg
+  mx-auto my-5 !mt-0
+  ">
+    <div className="flex space-x-3 w-full p-5 !pb-4">
       <span className="w-4 h-4 md:w-5 md:h-5 rounded-full bg-red-600" />
       <span className="w-4 h-4 md:w-5 md:h-5 rounded-full bg-yellow-600" />
       <span className="w-4 h-4 md:w-5 md:h-5 rounded-full bg-green-600" />
     </div>
-    <div>
-      <pre className=""
-        style={{
-
-        }}
-      >
-        {children}
-      </pre>
+    <div className="
+        overflow-x-scroll
+        p-5 !pt-0
+        space-y-5 w-full
+        font-mono text-lg md:text-xl
+        code-block pre-scrollbar
+        ">
+      <div>
+        <pre>
+          {children}
+        </pre>
+      </div>
     </div>
   </div>
 );
