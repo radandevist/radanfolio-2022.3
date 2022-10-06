9e0a743b746562f02c8ed65f74298d24fda961f4
Andrianarisoa Daniel <radandevist@gmail.com>
2022-10-06 19:30:01 +0300

    ✨ beuatiful language switcher

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,35 +1,113 @@
-import React/* , { useEffect } */ from "react";
-// import { useTranslation } from "next-i18next";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export const LanguageSwitcher = () => {
-  // const { i18n } = useTranslation();
-  const router = useRouter();
-  const { pathname, query, asPath/* , locale */ } = router;
+const languagesProperties: Record<string, any> = {
+  en: {
+    label: "🇬🇧 English",
+  },
+  fr: {
+    label: "🇫🇷 Français",
+  },
+  mg: {
+    label: "🇲🇬 Malagasy",
+  }
+};
 
-  // useEffect(() => {
-  //   i18n.changeLanguage(locale);
-  // }, [i18n, locale, i18n.language]);
+export const LanguageSwitcher = () => {
+  const router = useRouter();
+  const { pathname, query, asPath, locale } = router;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const node = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  const clickOutside = useCallback((event: Event) => {
+    if(node?.current?.contains(event.target as Node)) {
+      // click inside the scope
+      return;
+    } 
+    // outside click
+    setIsOpen(false);
+  }, []);
+
+  // Do something after component renders
+  useEffect(() => {
+    document.addEventListener("mousedown", clickOutside);
+
+    // clean up function before running new effect
+    return () => {
+      document.removeEventListener("mousedown", clickOutside);
+    };
+  }, [clickOutside]);
 
   return (
-    <div>
-      <p>Switch to:</p>
-      <Link href={{ pathname, query }} as={asPath} locale="en">
-        <button>
-          <a>English</a>
-        </button>
-      </Link>
-      <Link href={{ pathname, query }} as={asPath} locale="fr">
-        <button>
-          <a>Français</a>
-        </button>
-      </Link>
-      <Link href={{ pathname, query }} as={asPath} locale="mg">
-        <button>
-          <a>Malagasy</a>
-        </button>
-      </Link>
+    <div ref={node} className="relative inline-block text-left w-[7.6rem]">
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        // eslint-disable-next-line max-len
+        className="inline-flex w-full justify-center rounded-md p-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-brand1 focus:ring-offset-2 ring-offset-slate-100 dark:ring-offset-brand2"
+        id="menu-button"
+        aria-expanded="true"
+        aria-haspopup="true"
+      >
+        {languagesProperties[locale!].label}
+        {/* Heroicon name: mini/chevron-down */}
+        <svg
+          className="-mr-1 ml-2 h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20" fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            // eslint-disable-next-line max-len
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {/* <!--
+        Dropdown menu, show/hide based on menu state.
+
+        Entering: "transition ease-out duration-100"
+          From: "transform opacity-0 scale-95"
+          To: "transform opacity-100 scale-100"
+        Leaving: "transition ease-in duration-75"
+          From: "transform opacity-100 scale-100"
+          To: "transform opacity-0 scale-95"
+      --> */}
+      <div
+        // eslint-disable-next-line max-len
+        className={`${isOpen ? "" : "hidden"} absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+      >
+        <div className="py-1" role="none">
+          {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+          {Object.entries(languagesProperties).map(([language, properties]) => (
+            <Link
+              key={language}
+              href={{ pathname, query }}
+              as={asPath}
+              locale={language}
+              role="menuitem"
+            >
+              <a
+                className="text-gray-700 block px-4 py-2 text-sm"
+                onClick={toggleDropdown}
+              >
+                {properties.label}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { links } from "../data/links";
 import { Link } from "./Link";
 import Image from "next/image";import { useTheme } from "../contexts/theme";
-import { ThemeSwitchV3 } from "./ThemeSwitch";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 import { LanguageSwitcher } from "./LanguageSwitcher";
 
 export const Navbar = () => {
@@ -31,9 +31,10 @@ export const Navbar = () => {
             </Link>
           ))}
           {/* <ThemeSwitch label="Dark mode" onToggle={toggleTheme} checked={theme === "dark"} /> */}
-          <ThemeSwitchV3 onToggle={toggleTheme} isDark={theme === "dark"} className="relative top-[10px] lg:top-2" />
-          {/* <LanguageSwitcher /> */}
+          <LanguageSwitcher />
+          <ThemeSwitcher onToggle={toggleTheme} isDark={theme === "dark"} className="relative top-[10px] lg:top-2" />
         </div>
+        {/* Hamburger menu icon */}
         {/* <div className="inline-flex md:hidden cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
