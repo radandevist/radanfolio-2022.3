59d7532f8d8d18eb2235558a1bc27cdde1c44407
Andrianarisoa Daniel <radandevist@gmail.com>
2022-11-02 18:27:15 +0300

    🎉 propeller ads finally working out of the box everytime

--- src/components/PropellerAds.tsx
+++ src/components/PropellerAds.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import Script from "next/script";
 
 export const PropellerAds = () => (
@@ -7,8 +6,13 @@ export const PropellerAds = () => (
     {/* <Script async={true} data-cfasync="false" src="//arsnivyr.com/1?z=5489682" /> */}
 
     {/*  */}
-    <Script data-cfasync="false" type="text/javascript" src="/propellerad-1.js" strategy="lazyOnload"/>
-    {/* <Script src="/propellerad-2.js" strategy="lazyOnload"/> */}
-    {/*  */}
+    <Script
+      async={false}
+      data-cfasync="false"
+      type="text/javascript"
+      src="/propellerad-1.js"
+      strategy="lazyOnload"
+    />
+    <Script async={false} src="/propellerad-2.js" strategy="lazyOnload"/>
   </>
 );
