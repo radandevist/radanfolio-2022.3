cfb3be1ee77c2fd38848236bbf1860e57e5065fd
Andrianarisoa Daniel <radandevist@gmail.com>
2022-11-30 11:43:55 +0300

    adCash script tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,12 @@ export default function Document() {
         <script type="text/javascript" src="/infolinks-account-vars.js" />
         <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js" />
 
+        {/* AdCash Ads */}
+        {/* eslint-disable max-len */}
+        {/* @ts-ignore */}
+        <script data-cfasync="false" type="text/javascript" data-adel="atag" src="//acacdn.com/script/atg.js" czid="envpzwvahu" />
+        {/* eslint-enable max-len */}
+
         {/* eslint-enable @next/next/no-sync-scripts */}
       </body>
     </Html>
